8ee3124993e2692c7441078a4cb5b6036220fcb3
Ershat12 <ershat200@mail.ru>
2024-01-09 21:42:16 +0600

    init

---
+++ html and css/js/index.js
@@ -0,0 +1,18 @@
+var a = 100;
+a = 101;
+
+//console.log(a);
+
+let b = "string";
+
+b = "string2";
+
+console.log(b);
+
+
+
+const c = 255;
+ 
+c = 1440;
+
+console.log(c);
---
+++ index.js
@@ -0,0 +1,14 @@
+//document.querySelector("#heading-id").style.backgroundColor = "red";
+
+//const test = document.querySelector("#test");
+//const p = document.createElement("p");
+ 
+//test.textContent = "hello world";
+//const menuBtn = document.querySelector ("#menu-btn")
+//menuBtn.addEventListener ("click", )
+
+const accBtn1 = document.querySelector("#acc-btn1");
+
+accBtn1.addEventListener("click", () => {  
+   document.querySelector("#acc-text1").classList.toggle("active");
+})
